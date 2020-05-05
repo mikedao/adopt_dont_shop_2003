9783c4ea11b7b071f97d3d674821cef9739a9644
Jack Cullen <jpcullen20@gmail.com>
2020-05-05 15:12:34 -0600

    Add route to create a new shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -7,6 +7,21 @@ class SheltersController < ApplicationController
     @shelter = Shelter.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    shelter = Shelter.create({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip],
+      })
+    shelter.save
+    redirect_to '/shelters'
+  end
+
   def edit
     @shelter = Shelter.find(params[:id])
   end
