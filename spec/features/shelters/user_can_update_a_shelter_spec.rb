c556886142b0e2e26257ff9acfecb6032356606f
Jack Cullen <jpcullen20@gmail.com>
2020-05-05 17:53:06 -0600

    Change before(:all) to before(:each)

--- spec/features/shelters/user_can_update_a_shelter_spec.rb
+++ spec/features/shelters/user_can_update_a_shelter_spec.rb
@@ -2,7 +2,7 @@ require './spec/rails_helper'
 
 RSpec.describe "shelter edit page", type: :feature do
 
-  before(:all) do
+  before(:each) do
     @shelter1 = Shelter.create(name: "Angels With Paws", address: "2540 Youngfield St", city: "Lakewood", state: "CO", zip: "80215")
     @shelter2 = Shelter.create(name: "MaxFund Dog Shelter", address: "1005 Galapago", city: "Denver", state: "CO", zip: "80204")
   end
