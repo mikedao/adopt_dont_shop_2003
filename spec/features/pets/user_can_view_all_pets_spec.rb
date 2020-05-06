590ad27f889e5f6c6f917646db2285c70cb2f4df
Jack Cullen <jpcullen20@gmail.com>
2020-05-06 14:59:58 -0600

    Remove spec for image

--- spec/features/pets/user_can_view_all_pets_spec.rb
+++ spec/features/pets/user_can_view_all_pets_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe "pets index page", type: :feature do
 
   it "user can see each pet and information on the pets index page" do
     shelter = Shelter.create(name: "Angels With Paws")
-    pet = Pet.create(image_path: "https://imgur.com/t/dog/dMapjoS", name: "Bruno", approximate_age: "4", sex: "M", shelter_id: shelter.id)
+    pet = Pet.create(image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg", name: "Bruno", approximate_age: "4", sex: "M", shelter_id: shelter.id)
     visit "/pets"
-    expect(page).to have_content(pet.image_path)
+    
     expect(page).to have_content(pet.name)
     expect(page).to have_content(pet.approximate_age)
     expect(page).to have_content(pet.sex)
