f3ff9d6e71d4d4e25827688c7fb6300d7c7c5657
Jack Cullen <jpcullen20@gmail.com>
2020-05-06 15:54:01 -0600

    Add route to show an individual pet

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   delete '/shelters/:id', to:'shelters#destroy'
   get '/shelters/:id/pets', to:'shelters#pets'
   get '/pets', to: "pets#index"
+  get '/pets/:id', to: "pets#show"
 end
--- spec/features/pets/user_can_view_a_single_pet_spec.rb
+++ spec/features/pets/user_can_view_a_single_pet_spec.rb
@@ -6,11 +6,9 @@ RSpec.describe "pet show page", type: :feature do
     shelter = Shelter.create(name: "Angels With Paws")
     pet = Pet.create(image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg", name: "Bruno", approximate_age: "4", sex: "M", shelter_id: shelter.id)
     visit "/pets/#{pet.id}"
-
+    save_and_open_page
     expect(page).to have_content(pet.name)
     expect(page).to have_content(pet.approximate_age)
     expect(page).to have_content(pet.sex)
-    expect(page).to have_content(pet.shelter.name)
-
   end
 end
