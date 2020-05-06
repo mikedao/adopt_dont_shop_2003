f2d1260796591937003aa6e736eb2c5e66341edf
Jack Cullen <jpcullen20@gmail.com>
2020-05-06 14:36:58 -0600

    Add pet spec, model, and view

--- spec/features/pets/user_can_view_all_pets_spec.rb
+++ spec/features/pets/user_can_view_all_pets_spec.rb
@@ -3,12 +3,14 @@ require './spec/rails_helper'
 RSpec.describe "pets index page", type: :feature do
 
   it "user can see each pet and information on the pets index page" do
-    #pet = Pet.create(image: "", name: "", age: 4, sex: "M", shelter: "Angels With Paws")
+    pet = Pet.create(image_path: "https://imgur.com/t/dog/dMapjoS", name: "Bruno", approximate_age: "4", sex: "M", shelter: "Angels With Paws")
     visit "/pets"
-    expect(page).to have_content(pet.image)
+    expect(page).to have_content(pet.image_path)
     expect(page).to have_content(pet.name)
-    expect(page).to have_content(pet.age)
+    expect(page).to have_content(pet.approximate_age)
     expect(page).to have_content(pet.sex)
-    expect(page).to have_content(pet.shelter)
+    expect(page).to have_content(pet.shelter.name)
   end
 end
+
+#rails g migration CreatePet image_path:string name:string approximate_age:string sex:string shelter:string
