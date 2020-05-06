fd33b9147d31896fb01c89c97261b02e61be93db
Jack Cullen <jpcullen20@gmail.com>
2020-05-06 16:49:42 -0600

    Add adoption status to pet show view

--- spec/features/pets/user_can_view_a_single_pet_spec.rb
+++ spec/features/pets/user_can_view_a_single_pet_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe "pet show page", type: :feature do
 
   it "user can see information for one pet" do
     shelter = Shelter.create(name: "Angels With Paws")
-    pet = Pet.create(image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg", name: "Bruno", approximate_age: "4", sex: "M", shelter_id: shelter.id)
+    pet = Pet.create(image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
+                      name: "Bruno",
+                      approximate_age: "4",
+                      sex: "M",
+                      shelter_id: shelter.id,
+                      adoption_status: "Adoptable")
     visit "/pets/#{pet.id}"
     expect(page).to have_content(pet.name)
     expect(page).to have_content(pet.approximate_age)
     expect(page).to have_content(pet.sex)
+    expect(page).to have_content(pet.adoption_status)
   end
 end
