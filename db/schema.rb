9842ad9517086dd0877e19a0fccc945ace8ca175
Jack Cullen <jpcullen20@gmail.com>
2020-05-06 14:42:04 -0600

    Add shelter id as foreign key in pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200506203730) do
+ActiveRecord::Schema.define(version: 20200506204100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20200506203730) do
     t.string "name"
     t.string "approximate_age"
     t.string "sex"
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 20200506203730) do
     t.string "zip"
   end
 
+  add_foreign_key "pets", "shelters"
 end
