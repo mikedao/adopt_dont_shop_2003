6dde405a115b00b774b371979e10a5ff16cb95a7
Jack Cullen <jpcullen20@gmail.com>
2020-05-06 14:45:48 -0600

    Add shelter object to pet index spec

--- spec/features/pets/user_can_view_all_pets_spec.rb
+++ spec/features/pets/user_can_view_all_pets_spec.rb
@@ -3,7 +3,8 @@ require './spec/rails_helper'
 RSpec.describe "pets index page", type: :feature do
 
   it "user can see each pet and information on the pets index page" do
-    pet = Pet.create(image_path: "https://imgur.com/t/dog/dMapjoS", name: "Bruno", approximate_age: "4", sex: "M", shelter: "Angels With Paws")
+    shelter = Shelter.create(name: "Angels With Paws")
+    pet = Pet.create(image_path: "https://imgur.com/t/dog/dMapjoS", name: "Bruno", approximate_age: "4", sex: "M", shelter_id: shelter.id)
     visit "/pets"
     expect(page).to have_content(pet.image_path)
     expect(page).to have_content(pet.name)
