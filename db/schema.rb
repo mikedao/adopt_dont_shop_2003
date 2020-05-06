eb2b65584325b0b73039865a00a000684a8783c9
Jack Cullen <jpcullen20@gmail.com>
2020-05-06 16:45:58 -0600

    Create adoption status column in pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200506204100) do
+ActiveRecord::Schema.define(version: 20200506224301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20200506204100) do
     t.string "approximate_age"
     t.string "sex"
     t.bigint "shelter_id"
+    t.string "adoption_status"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
