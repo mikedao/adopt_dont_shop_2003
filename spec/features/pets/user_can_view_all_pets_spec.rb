a9e78215d289893f78121c01fc885f9dc0c9ecb3
Jack Cullen <jpcullen20@gmail.com>
2020-05-05 21:21:56 -0600

    Add route for pets index

--- spec/features/pets/user_can_view_all_pets_spec.rb
+++ spec/features/pets/user_can_view_all_pets_spec.rb
@@ -3,6 +3,12 @@ require './spec/rails_helper'
 RSpec.describe "pets index page", type: :feature do
 
   it "user can see each pet and information on the pets index page" do
-    pet = Pet.create
+    #pet = Pet.create(image: "", name: "", age: 4, sex: "M", shelter: "Angels With Paws")
+    visit "/pets"
+    expect(page).to have_content(pet.image)
+    expect(page).to have_content(pet.name)
+    expect(page).to have_content(pet.age)
+    expect(page).to have_content(pet.sex)
+    expect(page).to have_content(pet.shelter)
   end
 end
