28e650d91d1016598b5150f757d7d9d4355f1862
Jack Cullen <jpcullen20@gmail.com>
2020-05-06 19:13:19 -0600

    Add description to pets

--- spec/features/pets/user_can_view_a_single_pet_spec.rb
+++ spec/features/pets/user_can_view_a_single_pet_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe "pet show page", type: :feature do
     shelter = Shelter.create(name: "Angels With Paws")
     pet = Pet.create(image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
                       name: "Bruno",
+                      description: "Best dog ever.",
                       approximate_age: "4",
                       sex: "M",
                       shelter_id: shelter.id,
                       adoption_status: "Adoptable")
     visit "/pets/#{pet.id}"
     expect(page).to have_content(pet.name)
+    expect(page).to have_content(pet.description)
     expect(page).to have_content(pet.approximate_age)
     expect(page).to have_content(pet.sex)
     expect(page).to have_content(pet.adoption_status)
