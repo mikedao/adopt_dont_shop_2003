fb18d63b1c563a82488d0ff614bb25ef376880f1
Jack Cullen <jpcullen20@gmail.com>
2020-05-07 15:27:28 -0600

    Add spec to view pets index from any page

--- spec/features/pets/user_can_view_all_pets_spec.rb
+++ spec/features/pets/user_can_view_all_pets_spec.rb
@@ -2,12 +2,25 @@ require './spec/rails_helper'
 
 RSpec.describe "pets index page", type: :feature do
 
+  before :each do
+    @shelter = Shelter.create(name: "Angels With Paws")
+    @pet1 = Pet.create(
+      image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
+      name: "Bruno",
+      approximate_age: "4",
+      sex: "M",
+      shelter_id: @shelter.id)
+    @pet2 = Pet.create(
+      image_path: "https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_1280.jpg",
+      name: "Woody",
+      approximate_age: "2",
+      sex: "F",
+      shelter_id: @shelter.id)
+  end
+
   it "user can see each pet and information on the pets index page" do
-    shelter = Shelter.create(name: "Angels With Paws")
-    pet1 = Pet.create(image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg", name: "Bruno", approximate_age: "4", sex: "M", shelter_id: shelter.id)
-    pet2 = Pet.create(image_path: "https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_1280.jpg", name: "Woody", approximate_age: "2", sex: "F", shelter_id: shelter.id)
     visit "/pets"
-    
+
     expect(page).to have_content(pet1.name)
     expect(page).to have_content(pet1.approximate_age)
     expect(page).to have_content(pet1.sex)
@@ -16,5 +29,28 @@ RSpec.describe "pets index page", type: :feature do
     expect(page).to have_content(pet2.approximate_age)
     expect(page).to have_content(pet2.sex)
     expect(page).to have_content(pet2.shelter.name)
+  end
+
+  describe "user can visit the pets index from any page" do
+    it "can go to pet index from the shelters index" do
+      visit "/shelters"
+      click_link "All Pets"
+
+      expect(current_path).to eq("/pets")
+    end
+
+    it "can go to pet index from the shelters show page" do
+      visit "/shelters/#{@shelter.id}"
+      click_link "All Pets"
+
+      expect(current_path).to eq("/pets")
+    end
+
+    it "can go to pet index from the pet show page" do
+      visit "/pet/#{@pet.id}"
+      click_link "All Pets"
+
+      expect(current_path).to eq("/pets")
+    end
   end
 end
